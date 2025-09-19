4f02499c5875d392be49ba53b838e0d349c40875
Megaraj P <megaraj.student@gmail.com>
2025-09-19 15:08:28 +0530

    Time: 6 ms (71.91%), Space: 44.9 MB (32%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -12,34 +12,34 @@ class Solution {
         //     }
         // }
         // return stack.size()
-        // Set<Character> map = new HashSet<>();
-        // int left = 0 , max_len = 0 ;
-        // for(int right = 0 ; right < s.length();right++){
-        //     char ch = s.charAt(right);
-        //     while(map.contains(ch)){
-        //         map.remove(ch);
-        //         left++;
+        Set<Character>set = new HashSet<>();
+        int left = 0, maxLen = 0;
+        for (int right = 0; right < s.length(); right++) {
+        char ch = s.charAt(right);
+        while (set.contains(ch)) {
+            set.remove(s.charAt(left));
+            left++;
+        }
+        set.add(ch);
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+    return maxLen;
+
+        // Set<Character> set = new HashSet<>();
+        // int max_len = 0 ; 
+        // for(int i=0 ; i<s.length();i++){
+        //     int win_sum=0;
+        //     int j = i;
+        //     while(j<s.length()){
+        //     int var = set.size();
+        //     set.add(s.charAt(j));
+        //     if(var == set.size()) break;
+        //     else win_sum++;
+        //     j++;
         //     }
-        //     map.add(ch);
-        //     max_len = Math.max(max_len , right - left + 1 );
+        //     set.clear();
+        //     max_len = Math.max(max_len , win_sum);
         // }
         // return max_len;
-
-        Set<Character> set = new HashSet<>();
-        int max_len = 0 ; 
-        for(int i=0 ; i<s.length();i++){
-            int win_sum=0;
-            int j = i;
-            while(j<s.length()){
-            int var = set.size();
-            set.add(s.charAt(j));
-            if(var == set.size()) break;
-            else win_sum++;
-            j++;
-            }
-            set.clear();
-            max_len = Math.max(max_len , win_sum);
-        }
-        return max_len;
     }
 }
